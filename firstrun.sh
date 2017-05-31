7355042b867fa453413cbb66a396744995bccaac
Craig Huther <fajamushi@gmail.com>
2017-05-31 12:13:38 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -143,7 +143,7 @@ else
 fi
 
 service php5-fpm start
-cat /custom/customhosts >> /etc/hosts
+cat /config/customhosts >> /etc/hosts
 service nginx start
 if [ -S "/var/run/fail2ban/fail2ban.sock" ]; then
   echo "fail2ban.sock found, deleting"
